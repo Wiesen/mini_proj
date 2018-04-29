a8bc0b1f5502c2eb36a4401cb74b50fac12ec7c1
wiesen <yangwj92@zju.edu.cn>
2018-04-29 13:39:57 +0800

    added user table

--- src/livingserver/main.go
+++ src/livingserver/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-	_ "livingserver/routers"
-
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	"mini_proj/src/livingserver/models"
 )
 
+func initDB() {
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	orm.RegisterDataBase("default", "mysql", "root:root@/orm_test?charset=utf8")
+	orm.RegisterModel(
+		new(models.User))
+	orm.RunSyncdb("default", false, true)
+	//o := orm.NewOrm()
+	//o.Using("default")
+}
+
 func main() {
+	initDB()
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
--- src/livingserver/models/user.go
+++ src/livingserver/models/user.go
@@ -1,86 +1,73 @@
 package models
 
 import (
-	"errors"
-	"strconv"
-	"time"
+	"image"
+	"github.com/astaxie/beego/orm"
 )
 
-var (
-	UserList map[string]*User
-)
-
-func init() {
-	UserList = make(map[string]*User)
-	u := User{"user_11111", "astaxie", "11111", Profile{"male", 20, "Singapore", "astaxie@gmail.com"}}
-	UserList["user_11111"] = &u
-}
 
 type User struct {
-	Id       string
-	Username string
+	uId      uint64		// phone_number, unique
+	QQNumber uint64		// qq_number, unique
+	Username string		// nickname, unique
 	Password string
-	Profile  Profile
+	Avatar   image.Image
 }
 
-type Profile struct {
-	Gender  string
-	Age     int
-	Address string
-	Email   string
+func AddUser(user *User) int64 {
+	o := orm.NewOrm()
+	id, _ := o.Insert(user)
+	return id
 }
 
-func AddUser(u User) string {
-	u.Id = "user_" + strconv.FormatInt(time.Now().UnixNano(), 10)
-	UserList[u.Id] = &u
-	return u.Id
+func FindUserById(uId uint64) (bool, User) {
+	o := orm.NewOrm()
+	var user User
+	err := o.QueryTable(user).Filter("uId", uId).One(&user)
+	return err != orm.ErrNoRows, user
 }
 
-func GetUser(uid string) (u *User, err error) {
-	if u, ok := UserList[uid]; ok {
-		return u, nil
-	}
-	return nil, errors.New("User not exists")
+func FindUserByQQ(QQ uint64) (bool, User) {
+	o := orm.NewOrm()
+	var user User
+	err := o.QueryTable(user).Filter("QQNumber", QQ).One(&user)
+	return err != orm.ErrNoRows, user
 }
 
-func GetAllUsers() map[string]*User {
-	return UserList
+func FindUserByUserName(username string) (bool, User) {
+	o := orm.NewOrm()
+	var user User
+	err := o.QueryTable(user).Filter("Username", username).One(&user)
+	return err != orm.ErrNoRows, user
 }
 
-func UpdateUser(uid string, uu *User) (a *User, err error) {
-	if u, ok := UserList[uid]; ok {
-		if uu.Username != "" {
-			u.Username = uu.Username
-		}
-		if uu.Password != "" {
-			u.Password = uu.Password
-		}
-		if uu.Profile.Age != 0 {
-			u.Profile.Age = uu.Profile.Age
-		}
-		if uu.Profile.Address != "" {
-			u.Profile.Address = uu.Profile.Address
-		}
-		if uu.Profile.Gender != "" {
-			u.Profile.Gender = uu.Profile.Gender
-		}
-		if uu.Profile.Email != "" {
-			u.Profile.Email = uu.Profile.Email
-		}
-		return u, nil
-	}
-	return nil, errors.New("User Not Exist")
+func UpdateUser(user *User) (int64, error) {
+	o := orm.NewOrm()
+	return o.Update(user)
 }
 
-func Login(username, password string) bool {
-	for _, u := range UserList {
-		if u.Username == username && u.Password == password {
-			return true
-		}
-	}
-	return false
+func LoginByName(username string, password string) (bool, User) {
+	o := orm.NewOrm()
+	var user User
+	err := o.QueryTable(user).Filter("Username", username).Filter("Password", password).One(&user)
+	return err != orm.ErrNoRows, user
 }
 
-func DeleteUser(uid string) {
-	delete(UserList, uid)
+func LoginByUID(uId uint64, password string) (bool, User) {
+	o := orm.NewOrm()
+	var user User
+	err := o.QueryTable(user).Filter("uId", uId).Filter("Password", password).One(&user)
+	return err != orm.ErrNoRows, user
+}
+
+func LoginByQQ(QQ uint64, password string) (bool, User) {
+	o := orm.NewOrm()
+	var user User
+	err := o.QueryTable(user).Filter("QQNumber", QQ).Filter("Password", password).One(&user)
+	return err != orm.ErrNoRows, user
+}
+
+func DeleteUser(user *User) {
+	o := orm.NewOrm()
+	o.Delete(user)
 }
